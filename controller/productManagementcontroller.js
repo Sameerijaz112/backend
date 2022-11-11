f3ed6ca24a6fb9cd5fa4743dad319c04870814dd
Sameerijaz112 <sameerejaz970@gmail.com>
2022-11-11 01:19:25 -0800

    ok

---
+++ controller/productManagementcontroller.js
@@ -0,0 +1,61 @@
+const productModel = require('../model/productmodel')
+
+const ProductData = async (req, res) => {
+    try {
+        const {  productName, productQuantityy, productPrice,   description, logoMaterial, size, category, color, productMaterial } =req.body;
+        let ImageDetaisl = [];
+        let Size = size.split(',');
+        req.files.forEach(arrayObject => {
+            const { filename, originalname, mimetype } = arrayObject;
+            ImageDetaisl.push({
+                ImageUrl:`assets/Product/${productName}/${filename}`, 
+                ImageName:originalname, 
+                ImageMimeType:mimetype
+            });
+        })
+        const docToCreate = new ProductModel({
+            productName, productQuantityy, productPrice,   description, logoMaterial, size, category, color, productMaterial , 
+            imageDetaisl:ImageDetaisl
+        })
+
+        const docToSave = await docToCreate.save();
+        res.json({
+            Message:'Data Saved Successfuly',
+            Body:docToSave,
+            Data:true
+        })
+    } catch (error) {
+        res.json({
+            Message: error.mesage,
+            Result: null,
+            Data: false
+        })
+    }
+}
+
+const GetProductData = async (req, res) => {
+    try {
+        // const DocToGet = await ProductModel.findOne(
+        //     { Status: 0 }, //Condition
+        //     { ProductPrice: 0 } //Projecttion
+        //     //Options
+        // );
+        const docToGet = await ProductModel.find();
+        res.json({
+            Message: 'All Documents Found',
+            Data: true,
+            Result: docToGet
+        })
+    } catch (error) {
+        res.json({
+            Message: error.mesage,
+            Result: null,
+            Data: false
+        })
+    }
+}
+module.exports = {
+    ProductData,
+    GetProductData,
+   
+}
