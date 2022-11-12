b18d0559d0d51c0499fd8b66c241a8c4eb624a5f
Sameerijaz112 <sameerejaz970@gmail.com>
2022-11-12 01:49:48 -0800

    yes

--- Routes/productroutes.js
+++ Routes/productroutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const Router = express.Router();
 
 const { 
-    ProductData,
+    ProductData, GetProductData,
   
  }= require('../controller/productManagementcontroller')
 
  const {UploadProductImage}= require('../middleware/UploadMedia');
 
  Router.post('/ProductData',UploadProductImage.array('images',20),ProductData)
+ Router.get('/Getproduct',GetProductData )
 
  module.exports = Router;
